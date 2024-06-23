fe2024d3f3595f432acf707f06af5dc52d60b71d
Kabera Samuel <kaberanshutis@gmail.com>
2024-06-23 11:18:54 +0200

    importing files from Odin repository and answering the first exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const array2 = [];
+  for (let char of string.split("")) {
+    array2.unshift(char);
+  }
+  return array2.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, removable) {
+  if (!array.includes(removable)) return array;
+  array.splice(array.indexOf(removable), 1);
+  return array;
 };
 
 // Do not edit below this line
