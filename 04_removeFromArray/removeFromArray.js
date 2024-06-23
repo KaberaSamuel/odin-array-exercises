e9bf8ceb3a942de50831de5c75253491570796e7
Kabera Samuel <kaberanshutis@gmail.com>
2024-06-23 23:49:57 +0200

    Solutions for exercise 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,16 @@
-const removeFromArray = function (array, removable) {
-  if (!array.includes(removable)) return array;
-  array.splice(array.indexOf(removable), 1);
-  return array;
+// My Solution
+const removeFromArray = function (array, ...removables) {
+  const array2 = [];
+  for (let item of array) {
+    if (!removables.includes(item)) {
+      array2.push(item);
+    }
+  }
+
+  return array2;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7], 1, 2, 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (first, last) {
+  if (
+    (typeof first === "number") &
+    (typeof last === "number") &
+    (first > 0) &
+    (last > 0)
+  ) {
+    let sum = 0;
+    if (first < last) {
+      for (let counter = first; counter <= last; counter++) {
+        sum += counter;
+      }
+      return sum;
+    } else {
+      for (let counter = first; counter >= last; counter--) {
+        sum += counter;
+      }
+      return sum;
+    }
+  }
+  return "ERROR";
 };
 
 // Do not edit below this line
