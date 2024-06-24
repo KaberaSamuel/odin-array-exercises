4ab4d8d2d0051c421fa1360303157883faf7dc80
Kabera Samuel <kaberanshutis@gmail.com>
2024-06-24 15:29:16 +0200

    solution for exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const checkAlphabet = function (char) {
+  const alphabets = "abcdefghijklmnopqrstuvwxyz";
+  return alphabets.includes(char);
 };
 
+const palindromes = function (input) {
+  const input2 = input.toLowerCase();
+
+  let stringOriginal = input2.split("").filter(checkAlphabet).join("");
+
+  let reversedString = input2
+    .split("")
+    .filter(checkAlphabet)
+    .reverse()
+    .join("");
+
+  return stringOriginal === reversedString;
+};
+
+palindromes("r3ace3car");
 // Do not edit below this line
 module.exports = palindromes;
